2793d90f242faf35a17402a6e0558e67a294c9a5
sunbaoshi <sunbaoshi1975@gmail.com>
2019-11-30 02:13:36 -0500

    Update UsartDev.h

--- stm8l/Xlight-stm8l/UsartDev.h
+++ stm8l/Xlight-stm8l/UsartDev.h
@@ -5,6 +5,7 @@
 
 void UsartEnableRxTx(bool xRxEnable, bool xTxEnable);
 uint8_t UsartGetByte(uint8_t *pByte);
+void UsartPutByte(uint8_t data);
 uint8_t UsartSendByte(uint8_t data);
 void usart_config(uint32_t speed);
 uint8_t UsartSendString(uint8_t *pBuf);
