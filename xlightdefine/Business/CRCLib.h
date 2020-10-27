98b45c06426e793bf97f156682592cb510b7eb73
DataTellIt Commercial <bs.sun@datatellit.com>
2020-10-27 01:38:35 -0400

    add CRCLib

---
+++ xlightdefine/Business/CRCLib.h
@@ -0,0 +1,8 @@
+#ifndef _CRC_COMMON_LIB_H_
+#define _CRC_COMMON_LIB_H_
+
+#include "stm8l15x.h"
+
+uint8_t CRC8_Check(const uint8_t *pushMsg, const uint16_t usDataLen, const uint8_t ucCRCH, const uint8_t ucCRCL);
+
+#endif // _CRC_COMMON_LIB_H_
